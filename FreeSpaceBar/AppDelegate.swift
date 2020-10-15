c6a5eda46fa7400b2d1c3bb22b9f814af4b40834
David Messem <david@synthesis.co.za>
2020-10-15 19:18:03 +0200

    init

--- FreeSpaceBar/AppDelegate.swift
+++ FreeSpaceBar/AppDelegate.swift
@@ -12,28 +12,54 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
+    var statusBarItem: NSStatusItem!
+    var timer: Timer!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+        statusBarItem.button?.title = "..."
+        
+        let statusBarMenu = NSMenu(title: "Menu")
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.addItem(
+            withTitle: "Refresh",
+            action: #selector(AppDelegate.refresh),
+            keyEquivalent: "")
+        
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(refresh), userInfo: nil, repeats: true)
+        
+        refresh()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        timer.invalidate()
     }
-
-
+    
+    @objc func refresh() {
+        print("Ordering a burrito!")
+        let url = URL.init(fileURLWithPath: "/Volumes/Macintosh HD")
+        do {
+            let theVolSizeAvail = try url.resourceValues(forKeys: [.volumeSupportsVolumeSizesKey])
+            if let theVolSizeIsAvail = theVolSizeAvail.volumeSupportsVolumeSizes {
+                if theVolSizeIsAvail {
+                    let result = try url.resourceValues(forKeys: [.volumeAvailableCapacityKey, .volumeTotalCapacityKey, .volumeAvailableCapacityForImportantUsageKey, .volumeAvailableCapacityForOpportunisticUsageKey])
+                    if let freeBytes = result.volumeAvailableCapacityForImportantUsage {
+                        print("Free space on \(url.path) is \(freeBytes/1000000000) GB")
+                        statusBarItem.button?.title = "\(freeBytes/1000000000) GB"
+                    }
+                }
+            }
+        } catch {
+            print("Bad")
+        }
+    }
+    
+        
+    
 }
 
